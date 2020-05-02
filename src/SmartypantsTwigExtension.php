2d9fb631249ca37bc72b7e3837ec599d47ba5e87
TJ Draper <tj@buzzingpixel.com>
2020-05-02 15:18:07 -0500

    Update filter to be compatible with Twig 3

--- src/SmartypantsTwigExtension.php
+++ src/SmartypantsTwigExtension.php
@@ -3,18 +3,18 @@ declare(strict_types=1);
 
 /**
  * @author TJ Draper <tj@buzzingpixel.com>
- * @copyright 2018 BuzzingPixel, LLC
+ * @copyright 2020 BuzzingPixel, LLC
  * @license Apache-2.0
  */
 
 namespace buzzingpixel\twigsmartypants;
 
-use Twig_Filter;
-use Twig_Markup;
-use Twig_Extension;
+use Twig\TwigFilter;
+use Twig\Markup;
+use Twig\Extension\AbstractExtension;
 use Michelf\SmartyPants;
 
-class SmartypantsTwigExtension extends Twig_Extension
+class SmartypantsTwigExtension extends AbstractExtension
 {
     private $smartyPants;
 
@@ -25,11 +25,17 @@ class SmartypantsTwigExtension extends Twig_Extension
 
     public function getFilters(): array
     {
-        return [new Twig_Filter('smartypants', [$this, 'smartypantsFilter'])];
+        return [new TwigFilter(
+            'smartypants',
+            [$this, 'smartypantsFilter']
+        )];
     }
 
-    public function smartypantsFilter(string $str): Twig_Markup
+    public function smartypantsFilter(string $str): Markup
     {
-        return new Twig_Markup($this->smartyPants->transform($str), 'UTF-8');
+        return new Markup(
+            $this->smartyPants->transform($str),
+            'UTF-8'
+        );
     }
 }
